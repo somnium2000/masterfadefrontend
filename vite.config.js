ede4c5515cd16b92d82719d6c212dd995f8d88df
Andres Miranda <fpinedam@unah.hn>
2026-03-01 17:35:50 -0600

    Se agregaron librerias

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import million from "million/compiler";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [million.vite({ auto: true }), react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
